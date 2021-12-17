cfd075f70535565ae2bc19071b2cfe7e6aacd75f
Willian Carvalho <Willss_oak@hotmail.com>
2021-12-16 23:29:44 -0300

    treinando o transversing

--- js/main.js
+++ js/main.js
@@ -26,13 +26,46 @@
 
 // console.log(menu.getAttribute("data-item"));
 
-const menu = document.querySelectorAll(".menu a");
-console.log(menu);
-menu.forEach((item) => item.classList.add("ativo"));
-menu.forEach((item) => item.classList.remove("ativo"));
-menu[0].classList.add("ativo");
+// const menu = document.querySelectorAll(".menu a");
+// console.log(menu);
+// menu.forEach((item) => item.classList.add("ativo"));
+// menu.forEach((item) => item.classList.remove("ativo"));
+// menu[0].classList.add("ativo");
 
-const imagensAlt = document.querySelectorAll("img");
-imagensAlt.forEach((item) => console.log(item.hasAttribute("alt")));
+// const imagensAlt = document.querySelectorAll("img");
+// imagensAlt.forEach((item) => console.log(item.hasAttribute("alt")));
 
-document.querySelector('a[href^="http"]').setAttribute("href", "https://www.wicarvalho.com");
+// document.querySelector('a[href^="http"]').setAttribute("href", "https://www.wicarvalho.com");
+
+// //verificar distacia da primeira imagem ao topo
+// const primeiraImagem = document.querySelector("img");
+// console.log(primeiraImagem.offsetTop);
+
+// //retornar soma da largura das imagens
+// const imagens = document.querySelectorAll("img");
+// console.log(imagens[0].width);
+// let total = [...imagens].reduce((total, item) => {
+//   return total + item.offsetWidth;
+// }, 0);
+// console.log(total);
+
+// //verificar tamanho dos links
+// const links = document.querySelectorAll("a");
+// console.log(links[0].offsetWidth, links[0].offsetHeight);
+
+// //adicionar classe se o browser for menor que 700px
+// if (window.innerWidth < 700) {
+//   document.querySelector(".menu").classList.add("menor");
+// }
+
+// trabalhando com eventos
+// const menu = document.querySelector(".menu");
+
+// menu.addEventListener("click", (evento) => {
+//   event.preventDefault();
+//   console.log(evento);
+// });
+
+//transversing
+const lista = document.querySelector(".animais-lista");
+console.log(lista.parentNode);
